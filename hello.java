1aae572aad1a9a265bb365ae55a831062f0a28f0
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2022-04-13 16:59:54 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,5 +5,6 @@ public class hello {
 		System.out.println("HIHIHI@@");
 		System.out.println("sdfesfsdf");
 		System.out.println("#$s@d12314esfsdf");
+		System.out.println("#$s@d12314esfsdf");
 	}
 }
